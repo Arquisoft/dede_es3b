b95069765c3d9c9289c5e7678f30f792e02d6e6f
Daniel Pascual López <uo269728@uniovi.es>
2022-02-28 18:48:05 +0100

    Ya funciona el login

--- webapp/src/authentication/Authenticator.tsx
+++ webapp/src/authentication/Authenticator.tsx
@@ -1,76 +1,34 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import type { AlertColor } from '@mui/material/Alert';
-
-import {
-    getSolidDataset,
-    getThing,
-    setThing,
-    getStringNoLocale,
-    setStringNoLocale,
-    saveSolidDatasetAt
-} from "@inrupt/solid-client";
-import { Session } from "@inrupt/solid-client-authn-browser";
-
-
+import { useState } from "react";
+import LoginForm from "./LoginForm"
+import ProfileViewer from "./ProfileViewer"
+import { SessionProvider, useSession } from "@inrupt/solid-ui-react";
 
 
 
 
 
 function Authenticator(): JSX.Element {
-    // If your Pod is *not* on `solidcommunity.net`, change this to your identity provider.
-    const SOLID_IDENTITY_PROVIDER = "https://solidcommunity.net";
-    const session = new Session();
-    const buttonLogin = document.getElementById("btnLogin");
+    //We use this state variable
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    //With this we can control the login status for solid
+    const { session } = useSession();
 
-    // 1a. Start Login Process. Call session.login() function.
-    async function login() {
-        if (!session.info.isLoggedIn) {
-            await session.login({
-                oidcIssuer: SOLID_IDENTITY_PROVIDER,
-                redirectUrl: window.location.href
-            });
-        }else{
-            console.log('someone connected!');
-        }
-    }
- // 1b. Login Redirect. Call session.handleIncomingRedirect() function.
- // When redirected after login, finish the process by retrieving session information.
- async function handleRedirectAfterLogin() {
-    await session.handleIncomingRedirect(window.location.href);
-    if (session.info.isLoggedIn) {
-      // Update the page with the status.
-      console.log( session.info);
-    }
-  }
- 
-  // The example has the login redirect back to the index.html.
-  // This calls the function to process login information.
-  // If the function is called when not part of the login redirect, the function is a no-op.
-  handleRedirectAfterLogin();
+    //We have logged in
+    session.onLogin(() => {
+        setIsLoggedIn(true)
+    })
 
-    if(buttonLogin!= null){
-        buttonLogin.onclick = function () {
-            login();
-        };
-    }
+    //We have logged out
+    session.onLogout(() => {
+        setIsLoggedIn(false)
+    })
 
     return (
         <>
-            <section id="login" className="panel">
-                <div className="row">
-                    <label id="labelLogin" htmlFor="btnLogin"
-                    >1. Click the button to log into
-                        <span id="solid_identity_provider"
-                        >...provided by the JavaScript code...</span>:
-                    </label>
-                    <button name="btnLogin" id="btnLogin">Login</button>
-                    <p id="labelStatus" className="labelStatus"></p>
-                </div>
-            </section>
+            <SessionProvider sessionId="log-in-example">
+                {(!isLoggedIn) ? <LoginForm /> : <ProfileViewer />}
+            </SessionProvider>
         </>
     );
 }
