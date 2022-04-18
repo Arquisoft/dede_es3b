a906461a0a1eccc2a8e47c1331a85867f69198e8
José Rodríguez Rico <uo276922@uniovi.es>
2022-04-18 16:45:05 +0200

    Cambios en User para login

--- restapi/models/UserSchema.ts
+++ restapi/models/UserSchema.ts
@@ -1,24 +1,18 @@
 import { model, Schema} from 'mongoose'
 
+
+
 const userSchema = new Schema(
     {
-        dni: {
-            type: String,
-			required: true,
-			unique:true
-        },
-        name: {
-            type: String,
-            required: true
-        }, 
         email: {
             type: String,
             required: true
         },
-        surname: {
-            type: String,
+        password: {
+            type:String,
             required: true
         }
+
     }
 )
 
