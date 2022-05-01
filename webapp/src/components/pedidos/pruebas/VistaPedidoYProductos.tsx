a0f2342ea82b18a3898401cb305cce4ef33fdb61
Pablo <uo277348@uniovi.es>
2022-05-01 20:20:05 +0200

    nombre

--- webapp/src/components/pedidos/pruebas/VistaPedidoYProductos.tsx
+++ webapp/src/components/pedidos/pruebas/VistaPedidoYProductos.tsx
@@ -80,7 +80,7 @@ function VistaPedidoYProductos(order: OrderProducts): JSX.Element {
             <TableCell component="th" scope="row">
                 {order.order.name + " " + order.order.surname}
             </TableCell>
-            <TableCell >{order.order.email}</TableCell>
+            <TableCell >{order.order.pod_name}</TableCell>
             <TableCell >{order.order.pod_direction}</TableCell>
             <TableCell align="right">{order.order.price}</TableCell>
         </TableRow>
