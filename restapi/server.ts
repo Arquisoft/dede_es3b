229754e52c9746300db2e07971e5d452fc06052a
Daniel Pascual López <uo269728@uniovi.es>
2022-05-03 20:25:12 +0200

    Arreglo anterior

--- restapi/server.ts
+++ restapi/server.ts
@@ -25,14 +25,4 @@ app.listen(port, ():void => {
     console.error('Error occured: ' + error.message);
 });
 
-require("dotenv").config();
-const mongoose = require('mongoose')
-//prueba
-const uri = "mongodb+srv://DeDeportes3b:dedeportes2@aswdedeportes.9ukdb.mongodb.net/ASWDeDeportes?retryWrites=true&w=majority";
 
-mongoose.connect(uri)
-    .then(() => {
-        console.log('Conexion correcta a la BD')
-    }).catch((err:any) => {
-        console.log(err)
-    })
